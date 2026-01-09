aace1fb599d4e03e63e0730c5911dd09133e4147
idgodev <idgo.dev@gmail.com>
2026-01-09 14:02:37 +0700

    update artikel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // PENTING: Harus ada URL domain lu di sini biar Astro.site jalan
+  // Ganti https://diam.blog dengan domain asli lu kalau beda
+  site: 'https://diam.blog', 
+  
   integrations: [tailwind(), sitemap()],
 });
