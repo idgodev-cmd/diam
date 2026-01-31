38a16b59a0bda92c4decba944634ae07b2838562
idgodev <idgo.dev@gmail.com>
2026-01-31 14:56:59 +0700

    update 14

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/404",
-    "revision": "0.n05mbh8jos"
+    "revision": "0.7e6qm956el"
   }], {
     "directoryIndex": "index.html"
   });
